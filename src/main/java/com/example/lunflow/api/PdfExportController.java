76bfc7af7d06e55d24dad72ccd6fdcd984e887a6
eya <eyas92931@gmail.com>
2025-04-29 11:04:00 +0100

    export pdf  ajout image 29/04

--- src/main/java/com/example/lunflow/api/PdfExportController.java
+++ src/main/java/com/example/lunflow/api/PdfExportController.java
@@ -1,9 +1,13 @@
 package com.example.lunflow.api;
-import com.example.lunflow.Service.PdfExportService;
+import com.example.lunflow.Service.Impl.PdfExportService;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.ByteArrayInputStream;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+
 @RestController
 @RequestMapping("/export")
 public class PdfExportController {
@@ -14,20 +18,25 @@ public class PdfExportController {
         this.pdfExportService = pdfExportService;
     }
 
-    @PostMapping(value = "", produces = MediaType.APPLICATION_PDF_VALUE)
+    @PostMapping(value = "/pdf", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<byte[]> exportPdfFromContent(
-            @RequestBody Map<String, String> payload,
+            @RequestParam("content") String content,
+            @RequestParam("title") String title,
+            @RequestParam(value = "image", required = false) MultipartFile image,
             @RequestParam(defaultValue = "document") String filename) {
 
-        String content = payload.get("content");
-        String title = payload.getOrDefault("title", "Titre par défaut");
+        try (InputStream imageStream = (image != null && !image.isEmpty()) ? image.getInputStream() : null) {
 
-        ByteArrayInputStream pdfStream = pdfExportService.exportPdfFromContent(content, title);
+            ByteArrayInputStream pdfStream = pdfExportService.exportPdfFromContent(content, title, imageStream);
 
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + ".pdf\"")
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(pdfStream.readAllBytes());
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + ".pdf\"")
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(pdfStream.readAllBytes());
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
